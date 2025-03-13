eb342c732a49023b95a837aa476387b377de6cf8
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-13 13:50:02 -0300

    setting up the entities creation

---
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,20 @@
+package com.dailycodework.dreamshops.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private Category category;
+    private Image image;
+
+
+    private List<Image> images;
+
+}
