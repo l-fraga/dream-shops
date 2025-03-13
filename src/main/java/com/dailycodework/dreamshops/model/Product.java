897ec1f4a6355d3f455232f176f4d8b7e7f24c2c
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-13 15:19:24 -0300

    setting up the Category Entity relationships and database notations

--- src/main/java/com/dailycodework/dreamshops/model/Product.java
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -3,12 +3,13 @@ package com.dailycodework.dreamshops.model;
 import java.math.BigDecimal;
 import java.util.List;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,6 +31,8 @@ public class Product {
     private int inventory;
     private String description;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
     private Category category;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
