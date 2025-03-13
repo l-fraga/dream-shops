a1b85ee1a28fb4ba071825e5af6d9321e53d4fb9
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-13 15:02:02 -0300

    setting up the Product Entity relationships and database notations

--- src/main/java/com/dailycodework/dreamshops/model/Product.java
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -3,7 +3,26 @@ package com.dailycodework.dreamshops.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String brand;
@@ -12,9 +31,8 @@ public class Product {
     private String description;
 
     private Category category;
-    private Image image;
 
-
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> images;
 
 }
