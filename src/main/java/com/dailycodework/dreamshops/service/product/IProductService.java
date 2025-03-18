669d0b147a8d07df7023367427abc4e7a5187dc6
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-18 17:55:38 -0300

    add the service folder's structure

---
+++ src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
@@ -0,0 +1,31 @@
+package com.dailycodework.dreamshops.service.product;
+
+import com.dailycodework.dreamshops.model.Product;
+
+import java.util.List;
+
+
+public interface IProductService {
+        
+        Product getProductById(Long id);
+    
+        Product saveProduct(Product product);
+    
+        void deleteProductById(Long id);
+
+        void updateProduct(Product product, Long id);
+
+        List<Product> getAllProducts();
+
+        List<Product> getProductsByCategory(String category);
+
+        List<Product> getProductsByBrand(String brand);
+
+        List<Product> getProductsByCategoryAndBrand(String category, String brand);
+
+        List<Product> getProductsByName(String name);
+
+        List<Product> getProductsByBrandAndName(String brand, String name);
+
+        Long countProductsByBrandAndName(String brand, String name);
+}
