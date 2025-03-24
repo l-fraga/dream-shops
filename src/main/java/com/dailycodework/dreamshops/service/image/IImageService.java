6e4f10b85bad5aa876d361da27b0f8a3524a044d
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-24 16:08:42 -0300

    implemented service layer of category and image and created image dto

---
+++ src/main/java/com/dailycodework/dreamshops/service/image/IImageService.java
@@ -0,0 +1,17 @@
+package com.dailycodework.dreamshops.service.image;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dailycodework.dreamshops.dto.ImageDto;
+import com.dailycodework.dreamshops.model.Image;
+
+public interface IImageService {
+    
+    Image getImageById(Long id);
+    List<ImageDto> saveImage(List<MultipartFile> files, Long productId);
+    Image updateImage(MultipartFile file, Long imageId);
+    void deleteImageById(Long id);
+
+}
---
+++ src/main/java/com/dailycodework/dreamshops/service/image/ImageService.java
@@ -0,0 +1,89 @@
+package com.dailycodework.dreamshops.service.image;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dailycodework.dreamshops.Repository.ImageRepository;
+import com.dailycodework.dreamshops.dto.ImageDto;
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Image;
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.service.product.IProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService implements IImageService {
+
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
+    @Override
+    public Image getImageById(Long id) {
+        return imageRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No image found with id: " + id));
+    }
+
+    @Override
+    public List<ImageDto> saveImage(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+
+        List<ImageDto> savedImageDto = new ArrayList<>();
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                Image savedImage = imageRepository.save(image);
+
+                String downloadUrl = "api/v1/images/download/" + savedImage.getId();
+                savedImage.setDownloadUrl(downloadUrl);
+
+                savedImage = imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setId(savedImage.getId());
+                imageDto.setFileName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImageDto.add(imageDto);
+
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return savedImageDto;
+    }
+
+    @Override
+    public Image updateImage(MultipartFile file, Long imageId) {
+       Image image = getImageById(imageId);
+        try {
+            image.setFileName(file.getOriginalFilename());
+            image.setFileType(file.getContentType());
+            image.setImage(new SerialBlob(file.getBytes()));
+            return imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+       
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+        imageRepository.findById(id).ifPresentOrElse(imageRepository :: delete, () -> {
+            throw new ResourceNotFoundException("No image found with id: " + id);
+        });
+    }
+    
+    
+}
