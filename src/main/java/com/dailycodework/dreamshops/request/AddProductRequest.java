77a0a6077ebe4dfa20bcc332f8fcca100b4ae29b
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-24 16:05:16 -0300

    created product request to add and update products

---
+++ src/main/java/com/dailycodework/dreamshops/request/AddProductRequest.java
@@ -0,0 +1,18 @@
+package com.dailycodework.dreamshops.request;
+
+import java.math.BigDecimal;
+
+import com.dailycodework.dreamshops.model.Category;
+
+import lombok.Data;
+
+@Data
+public class AddProductRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
+}
