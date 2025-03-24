0d52096c545cdee1ff38be1c42a9524262509f80
Lucas Silva <lucassilvacma@hotmail.com>
2025-03-24 18:27:20 -0300

    configuring the api prefix and creating the image controller

---
+++ src/main/java/com/dailycodework/dreamshops/controller/ImageController.java
@@ -0,0 +1,8 @@
+package com.dailycodework.dreamshops.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("${api.prefix}/images")
+public class ImageController {
+    
+}
